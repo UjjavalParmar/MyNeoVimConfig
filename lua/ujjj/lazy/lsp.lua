7a8298eb8c63ad115b253cf83060a2a517f63873
Ujjj <ujjavalparmar2002@gmail.com>
2025-05-21 11:56:50 +0530

    minor

--- lua/ujjj/lazy/lsp.lua
+++ lua/ujjj/lazy/lsp.lua
@@ -43,7 +43,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
                 "gopls",
             },
             handlers = {
@@ -102,7 +101,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
